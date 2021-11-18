4b61b4f53e6901d20e8869131d09f2b1fd25003b
souvik <bca_02821402019souvikbhardwaj@jimsd.org>
2021-11-18 18:17:07 +0530

    commit

--- file35.js
+++ file35.js
@@ -3,3 +3,7 @@ const myArray = ["value1","value2"];
 let myarr1 = myArray[0];
 let myarr2 = myArray[1];
 console.log(`value of array1  is ${myarr1} and ${myarr2} `);
+
+  // let [myvar1,myvar2] = myArray; IS EQUAL TO let myarr1 = myArray[0];    
+                                        //let myarr2 = myArray[1];
+
