fb1e41f5ebbad32d94ea63099c06660673efad8f
souvik <bca_02821402019souvikbhardwaj@jimsd.org>
2021-11-18 18:24:42 +0530

    commit on  master

---
+++ file36.js
@@ -0,0 +1,8 @@
+//objects are reference type
+//array are good but not sufficicnet
+//objects store key value pair
+//objects dont have index
+//creating objects
+const person ={name:"Harshit" , age:22};
+// console.log(person);
+console.log(age);
